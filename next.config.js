9f500e3bb7185a41527fa409ef9242d220998291
ajaykanniyappan <ajaykanniyappan@gmail.com>
2022-11-29 18:33:43 +0530

    Tailwind Configurations

--- next.config.js
+++ next.config.js
@@ -2,13 +2,15 @@
 const runtimeCaching = require('next-pwa/cache');
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development', // 👈 DISABLING PWA IN DEVELOPMENT MODE
   register: true,
   skipWaiting: true,
   runtimeCaching
 });
 
-module.exports = withPWA({
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
 });
+
+module.exports = nextConfig;
