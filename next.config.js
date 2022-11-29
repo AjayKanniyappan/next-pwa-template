a66ac038134f87c45e3dadef288632db4e8ab3ad
ajaykanniyappan <ajaykanniyappan@gmail.com>
2022-11-29 16:48:30 +0530

    Eslint & Vscode configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require('next-pwa/cache');
 const withPWA = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching
 });
 
 module.exports = withPWA({
