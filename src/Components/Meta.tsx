5f867f6297cce94d958154583ec81ef20b92433e
ajaykanniyappan <ajaykanniyappan@gmail.com>
2022-12-02 23:49:36 +0530

    Meta tag changes

--- src/Components/Meta.tsx
+++ src/Components/Meta.tsx
@@ -9,16 +9,15 @@ export default function Meta() {
         content="Next.js, pwa, PWA-app, React, Electron, HTML, CSS, Sass, JavaScript, TypeScript"
       />
       <meta name="author" content="Ajay Kanniyappan" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <meta name="theme-color" content="#F3EFE0" media="(prefers-color-scheme: light)" />
+      <meta name="theme-color" content="#F3EFE0" media="(prefers-color-scheme: dark)" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-title" content="Next PWA Template" />
       <meta name="application-name" content="Next PWA Template" />
-      <link rel="icon" type="image/png" href="/images/favicon.ico" />
+      <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
       <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
       <link rel="manifest" href="/manifest.json" />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className="flex items-center justify-center h-screen">
-        <div className="text-black font-bold text-5xl">Hello World!</div>
+        <div className="text-black font-bold text-xl">Hello World!</div>
       </div>
     </div>
   );
