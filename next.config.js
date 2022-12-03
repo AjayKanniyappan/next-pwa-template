e8b94a01f35a63d63e82577e8e2d85edaab34bfa
ajaykanniyappan <ajaykanniyappan@gmail.com>
2022-12-03 18:58:06 +0530

    Next PWA Bug Fix

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const withPWA = require('next-pwa')({
 });
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = '/';
+let assetPrefix = ' ';
 let basePath = '';
 
 if (isGithubActions) {
